a5e9df50324783101427bc3fe33798f2de37edcb
Eloiza <eloiza.souza@zup.com.br>
2025-02-20 15:09:39 -0300

    chore: create Spring project

---
+++ src/main/java/com/zup/poe/PoeApplication.java
@@ -0,0 +1,13 @@
+package com.zup.poe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoeApplication.class, args);
+	}
+
+}
